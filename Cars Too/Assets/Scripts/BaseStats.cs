40f9c60edc62897883e053603b47d27cc79363d7
Kyle Jin Jones <kjjones1@uci.edu>
2019-08-14 15:45:14 -0700

    Base Stats Scriptable Object

--- Cars Too/Assets/Scripts/BaseStats.cs
+++ Cars Too/Assets/Scripts/BaseStats.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseStats : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+//Base Stats is a Scriptable Object class that stores base values, and other preset values for cars, this is used to maintain a constant baseline whenever the game is started for each class.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(fileName = "BaseStats.asset", menuName = "Capstone/BaseStats")]
+public class BaseStats : ScriptableObject
+{
+    public GameObject carmodel;
+
+    public float power = 10;
+    public float engine = 10;
+    public float handling = 10;
+    public float weight = 10;
+    public float hp = 100;
+    public float armor = 10;
+
+
+    [TextArea(3, 100)]
+    public string description;
+    
+
 }
