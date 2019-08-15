3ef1a3b3f249d00f248310a7293b1435139d6688
Kyle Jin Jones <kjjones1@uci.edu>
2019-08-14 18:45:45 -0700

    Abilities and Stats

---
+++ Cars Too/Assets/Scripts/Abilities.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Abilities stores what abilities are currently available, and unavailable, as well as which abilities are equipped. 
+public class Abilities : MonoBehaviour
+{
+    //The player's currently equipped abilities
+    public List<Ability> equipped;
+
+    //Abilities that are available but not equipped
+    public List<Ability> available;
+   
+    //Abilities that have not been unlocked yet
+    public List<Ability> locked;
+
+    public void InstantiateAbilities(List<AbilityBaseStats> abils)
+    {
+        //Instantiates the ability lists
+        equipped = new List<Ability>();
+        available = new List<Ability>();
+        locked = new List<Ability>();
+
+        //loads in the abilities
+        for (int i= 0; i < abils.Count; i++)
+        {
+            Ability temp = new Ability(abils[i]);
+            
+            //adds the first three to equipped, and any excess to locked to be unlocked later
+            if (i < 3)
+            {
+                equipped.Add(temp);
+            }
+            else
+            {
+                locked.Add(temp);
+            }  
+        }
+
+    }
+
+    //Swaps two abilities with the given names, assumes that one is in available, and the other is in equipped
+    public void SwapAbilities(string abilityname1, string abilityname2)
+    {
+        //This bool is used to indicate which was found in equipped and thus no longer needs to be searched for
+        bool first=true;
+
+        //Searches through equipped for both abilities
+        for(int i= 0; i < equipped.Count; i++)
+        {
+            if (equipped[i].abilityname == abilityname1)
+            {
+                //adds the abilitiy to the other list then removes it from this one, and sets the bool to the correct value
+                available.Add(equipped[i]);
+                equipped.RemoveAt(i);
+                first = true;
+                break;
+            }
+            else if (equipped[i].abilityname == abilityname2)
+            {
+                available.Add(equipped[i]);
+                equipped.RemoveAt(i);
+                first = false;
+                break;
+            }
+        }
+
+
+
+        for (int i = 0; i < available.Count; i++)
+        {
+            if (!first&&available[i].abilityname == abilityname1)
+            {
+                equipped.Add(available[i]);
+                available.RemoveAt(i);
+                break;
+            }
+            else if (first&&available[i].abilityname == abilityname2)
+            {
+                equipped.Add(available[i]);
+                available.RemoveAt(i);
+                break;
+            }
+        }
+
+    }
+
+    //Unlocks and Upgrades abilities based on levelupbonuses
+    public void UpgradeandUnlock(LevelUpBonuses lb)
+    {
+        //if there is a new ability to be added, check all locked abilities and the one with the same name to available
+        if (lb.newability != null)
+        {
+            foreach(Ability a in locked){
+                if (a.abilityname == lb.newability)
+                {
+                    available.Add(a);
+                    locked.Remove(a);
+                    break;
+                }
+            }
+        }
+
+        //if there is an ability to be upgraded search available and equipped for it, and upgrade it
+        if (lb.upgradedability != null)
+        {
+            foreach(Ability a in available)
+            {
+                if (a.abilityname == lb.upgradedability)
+                {
+                    a.Upgrade();
+                    break;
+                }
+            }
+
+            foreach(Ability a in equipped)
+            {
+                if (a.abilityname == lb.upgradedability)
+                {
+                    a.Upgrade();
+                    break;
+                }
+            }
+        }
+    }
+
+
+
+}
+
+//Class that holds the "live version" of ability stats that is changeable
+public class Ability
+{
+    public string abilityname = "PLACEHOLDER";
+    public float damagemodifier = 2.5f;
+    public float upgradeamt = .5f;
+    public string abilitydescription = "PLACEHOLDER_TEXT";
+
+    public Ability(AbilityBaseStats abs)
+    {
+        abilityname = abs.abilityname;
+        damagemodifier = abs.damagemodifier;
+        upgradeamt = abs.upgradeamt;
+        abilitydescription = abs.abilitydescription;
+
+    }
+
+    public void Upgrade()
+    {
+        damagemodifier += upgradeamt;
+    }
+
+}
---
+++ Cars Too/Assets/Scripts/Stats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//The stats class stores a "live version" of player stats that is manipulated by level up bonuses, and potentially temporarily by abilities
+public class Stats : MonoBehaviour
+{
+    //The stats are stored as public for easier access, but cannot be statics as that would interfere with other class's stats
+    public float power = 10;
+    public float engine = 10;
+    public float handling = 10;
+    public float weight = 10;
+    public float hp = 100;
+    public float armor = 10;
+
+    //initializes the stats class to the basestats given
+    void InitializeStats(BaseStats bs)
+    {
+        power = bs.power;
+        engine = bs.engine;
+        handling = bs.handling;
+        weight = bs.weight;
+        hp = bs.hp;
+        armor = bs.armor;
+    }
+
+    //Upgrades Stats based on the levelup bonuses provided
+    void UpgradeStats(LevelUpBonuses lb)
+    {
+        power += lb.powerchange;
+        engine += lb.enginechange;
+        handling += lb.handlingchange;
+        weight += lb.weightchange;
+        hp += lb.hpchange;
+        armor += lb.armorchange;
+    }
+}
