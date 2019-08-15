03ae6247e868d109984289e1a388869be3c8cfb4
Xavier Chua <xavierc.1998@gmail.com>
2019-08-15 15:46:41 -0700

    Simplified camera follow

--- Cars Too/Assets/Scripts/SimpleCameraFolow.cs
+++ Cars Too/Assets/Scripts/SimpleCameraFolow.cs
@@ -5,27 +5,14 @@ using UnityEngine;
 public class SimpleCameraFolow : MonoBehaviour
 {
     [SerializeField] Transform target;
-    Vector3 camDesiredTarget;
-    float maxDistance = 15f;
+    public float hoverDistance = 6f;
+    public float maxDistance = 15f;
 
     void LateUpdate()
     {
         transform.position = target.position;
         Quaternion targetRotation = Quaternion.Euler(0, target.rotation.eulerAngles.y, 0);
         transform.rotation = targetRotation;
-        transform.Translate(new Vector3(0, 6, -maxDistance));
-
-        RaycastHit hit;
-        var camVector = transform.position - target.position;
-        Ray ray = new Ray(target.position, camVector);
-        if (Physics.Raycast(ray, out hit, maxDistance + 0.5f))
-        {
-            transform.position = hit.point + hit.normal;
-        }
-
-        var rot = transform.rotation.eulerAngles;
-        rot.x = Vector3.Angle(target.position - transform.position, transform.forward);
-        transform.rotation = Quaternion.Euler(rot);
-        transform.Translate(Vector3.forward * 0.5f);
+        transform.Translate(new Vector3(0, hoverDistance, -maxDistance));
     }
 }
