5fe1078eedccc635849d1b12d5af96221ca31528
Kyle Jin Jones <kjjones1@uci.edu>
2019-08-19 17:29:56 -0700

    Exp, stats updates, and entity base class

--- Cars Too/Assets/Scripts/Exp.cs
+++ Cars Too/Assets/Scripts/Exp.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Exp : MonoBehaviour
+public class Exp 
 {
-    // Start is called before the first frame update
-    void Start()
+    public float currentxp = 0.0f;
+    public float maxxp = 100.0f;
+    public int level = 1;
+
+    //Adjustable numbers that determine the max level, and the increase of the exp requirement per level
+    float xpincrementer = 1.5f;
+    int maxlevel = 10;
+
+    //Adds Xp, returns true if a levelup occured as a result
+    //Otherwise returns false
+    public bool AddExp(float amt)
     {
-        
+        if (MaxLevel())
+        {
+            return false;
+        }
+
+        currentxp += amt;
+
+        //Checks for levelup
+        if (currentxp >= maxxp)
+        {
+            //checks if maxlevel has been reached
+            level++;
+            if (MaxLevel())
+            {
+                currentxp = 0;
+                maxxp = 0;
+            }
+            else
+            {
+                //if not at maxlevel, maintains excess xp, and increments max xp
+                currentxp -= maxxp;
+                maxxp *= xpincrementer;
+            }
+            return true;
+        }
+
+
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public bool MaxLevel()
     {
-        
+        return level == maxlevel;
     }
+
 }
