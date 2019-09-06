bada259d4ac032df60f53333e356ebdf6cea6407
Xavier Chua <xavierc.1998@gmail.com>
2019-09-06 12:17:43 -0700

    added navmesh components and simple follow behavior

---
+++ Cars Too/Assets/Scripts/EnemyNavMesh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavMesh : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);    
+    }
+}
