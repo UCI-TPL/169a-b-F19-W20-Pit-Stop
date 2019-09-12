ff8036160ebb17d9d9d9ce54618ae3583b1ff18f
Kyle Jin Jones <kjjones1@uci.edu>
2019-09-12 14:39:40 -0700

    added ability baseclass and finished saws

---
+++ Cars Too/Assets/Scripts/Abilities/AbilityEnabler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityEnabler : MonoBehaviour
+{
+    //This is a base class for all abilities to inherit from and just serves as a baseline by which
+    //ability data can be transmitted to the ability prefab
+
+    public Ability abil=null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //sets the Ability equal to the given ability
+    public void setAbility(Ability a)
+    {
+        abil = a;
+    }
+}
