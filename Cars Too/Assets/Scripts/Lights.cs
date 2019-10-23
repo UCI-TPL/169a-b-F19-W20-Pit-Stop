95d93bafc550ae473954e4aa8ec6a79dfb8353f7
Kyle Jin Jones <kjjones1@uci.edu>
2019-10-23 01:39:35 -0700

    Added Piper to Lvl1

---
+++ Cars Too/Assets/Scripts/Lights.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    public GameObject lights= null;
+
+    void Start()
+    {
+        lights.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            lights.SetActive(!lights.activeSelf);
+        }
+    }
+}
