06d895a2112c90a1a91b3566be0576482c99b419
Xavier Chua <xavierc.1998@gmail.com>
2019-10-26 14:22:08 -0700

    Added floor, boost unlockable, respawn
    - Floor prefab has a trigger collider under the map that respawns the car when they enter it.
    - Car movement has boost unlocked by default, holding shift increases the boost until a certain speed.
    - Moved inventory script to ui folder
    - Car respawns when you press "r"

---
+++ Cars Too/Assets/Scripts/UI/InventoryPanel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class InventoryPanel : MonoBehaviour
+{
+
+    //private PlayerEntity playerScript;
+
+    [SerializeField] GameObject inventory;
+
+    [SerializeField] TextMeshProUGUI giftOneText;
+    [SerializeField] TextMeshProUGUI giftTwoText;
+    [SerializeField] TextMeshProUGUI giftThreeText;
+    [SerializeField] TextMeshProUGUI giftFourText;
+    [SerializeField] TextMeshProUGUI giftFiveText;
+    [SerializeField] TextMeshProUGUI carPartsText;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DataManager.instance.carPartAcquired.AddListener(OnPartAcquired);
+        DataManager.instance.giftAcquired.AddListener(OnGiftAcquired);
+
+        inventory.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            Display();
+        }
+        else
+        {
+            Close();
+        }
+    }
+
+    void OnPartAcquired()
+    {
+        Debug.Log("Increase car parts");
+        carPartsText.text = string.Format("x {0}",DataManager.instance.GetCarParts());
+    }
+
+    void OnGiftAcquired()
+    {
+        Debug.Log("Increase gifts");
+        giftOneText.text = string.Format("x {0}", DataManager.instance.GetGiftCount(PresentType.one));
+        giftTwoText.text = string.Format("x {0}", DataManager.instance.GetGiftCount(PresentType.two));
+        giftThreeText.text = string.Format("x {0}", DataManager.instance.GetGiftCount(PresentType.three));
+        giftFourText.text = string.Format("x {0}", DataManager.instance.GetGiftCount(PresentType.four));
+        giftFiveText.text = string.Format("x {0}", DataManager.instance.GetGiftCount(PresentType.five));
+    }
+
+    public void Display()
+    {
+        inventory.SetActive(true);
+    }
+
+    public void Close()
+    {
+        inventory.SetActive(false);
+    }
+}
