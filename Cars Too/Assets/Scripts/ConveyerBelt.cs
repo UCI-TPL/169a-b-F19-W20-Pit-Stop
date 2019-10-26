e4131168ea9967352948a531fdfef3a4648e775f
Kyle Jin Jones <kjjones1@uci.edu>
2019-10-26 16:06:03 -0700

    Started lvl2 and made conveyerbelts

---
+++ Cars Too/Assets/Scripts/ConveyerBelt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+    [SerializeField] private Transform endpoint;
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+    }
+}
