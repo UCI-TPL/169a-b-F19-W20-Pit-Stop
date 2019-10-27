a35441980826ef8ceb59d519ac752ca20a793262
Kyle Jin Jones <kjjones1@uci.edu>
2019-10-27 01:34:39 -0700

    Added Boxes

---
+++ Cars Too/Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    [SerializeField] private float timer = 1.0f;
+    private float maxtimer = 1.0f;
+    [SerializeField] GameObject box = null;
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        maxtimer = timer;
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = maxtimer;
+            Instantiate(box, this.transform);
+        }
+    }
+}
