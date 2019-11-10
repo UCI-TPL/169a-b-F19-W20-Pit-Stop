2b77bf4a336188da8d421a00a7e81cd34614214e
Kyle Jin Jones <kjjones1@uci.edu>
2019-11-10 09:58:09 -0800

    chief ui

---
+++ Cars Too/Assets/Scripts/UI/ChiefUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChiefUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //static Dictionary<int, bool> conditions = new Dictionary<int, bool>();
+    public int numcarpartsneeded = 3;
+    [SerializeField] GameObject uipopup = null;
+    void Start()
+    {
+        uipopup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DataManager.instance.carParts >= numcarpartsneeded)
+        {
+            uipopup.SetActive(true);
+        }
+    }
+
+    
+}
