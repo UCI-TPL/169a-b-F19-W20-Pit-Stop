f345627bc0449b34c1994d1b911bcc74e6e8064d
Kyle Jin Jones <kjjones1@uci.edu>
2019-11-13 02:00:26 -0800

    tuning, and fixed bgm bug

---
+++ Cars Too/Assets/Scripts/BGChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGChanger : MonoBehaviour
+{
+    //switches to designated bgm on object becoming awake
+    [SerializeField] int songnumber = 5;
+    private void Awake()
+    {
+        DataManager.instance.am.PlayandTrackBGM(songnumber);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
