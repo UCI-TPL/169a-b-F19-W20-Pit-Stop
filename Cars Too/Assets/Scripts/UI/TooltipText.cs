80396c8406fe4ea9ba540586f8c3307299a920c0
Kyle Jin Jones <kjjones1@uci.edu>
2019-11-19 10:02:01 -0800

    Tooltips

---
+++ Cars Too/Assets/Scripts/UI/TooltipText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TooltipText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI tooltip = null; //text where tooltip goes
+    [SerializeField] private GameObject tooltipobjs = null; //empty that contains all tooltip parts for setting on and off
+    // Start is called before the first frame update
+    void Start()
+    {
+        tooltipobjs.SetActive(false);
+    }
+
+    public void DisplayTooltip(string txt)
+    {
+        tooltipobjs.SetActive(true);
+        tooltip.text = txt;
+    }
+
+    public void TurnOffTooltip()
+    {
+        tooltipobjs.SetActive(false);
+    }
+}
---
+++ Cars Too/Assets/Scripts/UI/TooltipTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipTrigger : MonoBehaviour
+{
+    [SerializeField] private string tooltip = "WASD to MOVE";
+    private TooltipText tt = null;
+
+    private void Start()
+    {
+        if (tt == null)
+        {
+            tt = GameObject.FindObjectOfType<TooltipText>();
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            tt.DisplayTooltip(tooltip);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            tt.TurnOffTooltip();
+        }
+    }
+
+   
+}
