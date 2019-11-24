64ffb4ea46e11e931539175ce0f206de13529111
Kyle Jin Jones <kjjones1@uci.edu>
2019-11-23 16:19:41 -0800

    Implemented Event1

---
+++ Cars Too/Assets/Scripts/HelperScripts/Event1Trigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Event1Trigger : Id
+{
+
+    [SerializeField] private string destscene = "Event1"; //Scene to transition to
+                                                          //Need to save player location, and potentially account for better settings for other events
+                                                          //Once other event triggers are known.
+
+
+    void Update()
+    {
+        //Might need to have an additional check to see if player is grounded Otherwise transitioning back will cause the player to fall
+        if (DataManager.instance.carParts >= 2)
+        {
+            SceneManager.LoadScene(destscene);
+        }
+    }
+}
