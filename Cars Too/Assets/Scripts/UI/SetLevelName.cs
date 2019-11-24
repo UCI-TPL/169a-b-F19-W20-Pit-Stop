a55be3b0ab3a48630652734098234327a77d7e94
Kyle Jin Jones <kjjones1@uci.edu>
2019-11-23 21:47:47 -0800

    Levelname

---
+++ Cars Too/Assets/Scripts/UI/SetLevelName.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SetLevelName : MonoBehaviour
+{
+    [SerializeField] private string LevelName = "The Factory";
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TextMeshProUGUI>().text = LevelName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
