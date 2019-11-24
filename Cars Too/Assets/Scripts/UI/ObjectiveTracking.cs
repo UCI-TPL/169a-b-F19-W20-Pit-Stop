a92097f9ff5d456ec83f16e2cadf6ae1b03fb688
Kyle Jin Jones <kjjones1@uci.edu>
2019-11-23 22:16:38 -0800

    Primitive Objective Tracking

---
+++ Cars Too/Assets/Scripts/UI/ObjectiveTracking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectiveTracking : MonoBehaviour
+{
+    private TextMeshProUGUI objtext = null;
+    public int partsneeded = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objtext = GetComponent<TextMeshProUGUI>();
+        DataManager.instance.carPartAcquired.AddListener(UpdateQuest);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void UpdateQuest()
+    {
+        objtext.text = "Gather Parts: " + DataManager.instance.carParts + "/"+partsneeded;
+    }
+
+    public void SetPartsNeeded(int n)
+    {
+        partsneeded = n;
+        UpdateQuest();
+    }
+}
