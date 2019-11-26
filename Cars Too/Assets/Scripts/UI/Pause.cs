cd22fae1c89f69408947b7ee6c0027b596650346
Kyle Jin Jones <kjjones1@uci.edu>
2019-11-26 15:18:33 -0800

    pausemenu

---
+++ Cars Too/Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    private CarMovement carmov = null;
+    [SerializeField] private GameObject pm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        carmov = GameObject.FindObjectOfType<CarMovement>();
+        pm.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pm.activeSelf)
+            {
+                ClosePauseMenu();
+            }
+            else
+            {
+                OpenPauseMenu();
+            }
+        }
+    }
+
+    public void OpenPauseMenu()
+    {
+        carmov.Pause();
+        pm.SetActive(true);
+    }
+
+    public void ClosePauseMenu()
+    {
+        carmov.Unpause();
+        pm.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
