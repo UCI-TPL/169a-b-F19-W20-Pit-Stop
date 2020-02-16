ad80ab9eafde1605cfefd6f42a527c39cb522fed
Kyle Jin Jones <kjjones1@uci.edu>
2020-02-16 13:30:44 -0800

    Dex Expressions

---
+++ Cars Too/Assets/Scripts/UI/DialogueLog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueLog : MonoBehaviour
+{
+    private string textlog = "";
+    [SerializeField] private TextMeshProUGUI log;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        log.text = textlog;
+    }
+
+    public void UpdateLog(string n, string d)
+    {
+        if (textlog != "")
+        {
+           // textlog += "\n";
+        }
+        textlog =n + "\n" + d+"\n"+"\n"+textlog;    
+    }
+}
