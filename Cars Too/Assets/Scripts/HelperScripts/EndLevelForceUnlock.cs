b9102bdc3cca4e7d7faca2fbd70615feaa31287d
Kyle Jin Jones <kjjones1@uci.edu>
2020-03-04 18:33:20 -0800

    LVL2P3, and EndLevel

---
+++ Cars Too/Assets/Scripts/HelperScripts/EndLevelForceUnlock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelForceUnlock : MonoBehaviour
+{
+    // Used to force unlock abilities in the endlevel
+    void Start()
+    {
+        DataManager.instance.canDestroy = true;
+        DataManager.instance.canThrow = true;
+        DataManager.instance.canHack = true;
+        DataManager.instance.canBoost = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
